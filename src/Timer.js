820d12e363b13dcc0f711dab3e8daa0a9debc085
mar1n <30738216+mar1n@users.noreply.github.com>
2019-11-16 15:25:20 +0000

    Timer component change for function component

--- src/Timer.js
+++ src/Timer.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import Time from './Timerset';
 
-class Timer extends React.Component {
-    render() {
-        const timer = this.props.time.map((time) => 
+const Timer = (props) => {
+        const timer = props.time.map((time) => 
             <div key={time.title} className='timer'>
                 <Time
                     title={time.title}
                     project={time.project}
                     elapsed={time.elapsed}
                     runningSince={time.runningSince}
-                    onStartClick={this.props.onStartClick}
-                    onStopClick={this.props.onStopClick}
-                    onResetClick={this.props.onResetClick}
-                    onLapClick={this.props.onLapClick}
+                    onStartClick={props.onStartClick}
+                    onStopClick={props.onStopClick}
+                    onResetClick={props.onResetClick}
+                    onLapClick={props.onLapClick}
                 />
             </div>
         );
-        const laps = this.props.laps.map((laps, index) =>
+        const laps = props.laps.map((laps, index) =>
             <li key={index}>
                 {++index} Lap {laps}
             </li>
@@ -28,7 +27,6 @@ class Timer extends React.Component {
                 <ul>{laps}</ul>
             </>
         );
-    }
 }
 
 export default Timer;
